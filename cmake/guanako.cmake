6060095a093e245587c65663f593ebe906e16da8
Dominique Lasserre <lasserre.d@gmail.com>
2013-01-22 23:09:58 +0100

    Make cmake code more generic (name all source/vapi files explicitly).
     * Add guanako/ to Valama project.

--- cmake/guanako.cmake
+++ cmake/guanako.cmake
@@ -1,11 +1,21 @@
-set(project_name Guanako)
-set(Guanako_VERSION 1.0)
-set(soversion 1)
+set(project_name "Guanako")
+set(Guanako_VERSION "1.0")
+set(soversion "1")
 set(required_pkgs
-gobject-2.0
-glib-2.0
-gee-1.0
-libvala-0.16
-libvala-0.18
-gtk+-3.0
+"gobject-2.0"
+"glib-2.0"
+"gee-1.0"
+"libvala-0.16"
+"libvala-0.18"
+"gtk+-3.0"
+)
+set(srcfiles
+"guanako_auto_indent.vala"
+"guanako_helpers.vala"
+"guanako_iterators.vala"
+"guanako.vala"
+"guanako_vapi_discoverer.vala"
+)
+set(vapifiles
+"config.vapi"
 )
