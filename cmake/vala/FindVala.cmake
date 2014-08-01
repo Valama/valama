Valama/valama

commit 19347ef2c5eeb788f075e83d1e9e94fc3f402ab9

remove unsupported vala version, vala-0.26 added. fix #128. fix super source view for Gtk >= 3.12. translations updated.

cmake/vala/FindVala.cmake
@@ -73,7 +73,7 @@
 ##
 
 # Search for the valac executable in the usual system paths.
-find_program(VALA_EXECUTABLE NAMES "valac" "valac-0.22" "valac-0.20" "valac-0.18" "valac-0.16" "valac-0.14" "valac-0.12")
+find_program(VALA_EXECUTABLE NAMES "valac-0.26" "valac-0.24" "valac-0.22" "valac-0.20" "valac")
 mark_as_advanced(VALA_EXECUTABLE)
 
 # Determine the valac version
@@ -94,6 +94,10 @@ if(VALA_EXECUTABLE)
     math(EXPR min_ver "${min_ver} + 1")
   endif()
   set(VALA_SHORTVER "${maj_ver}.${min_ver}" CACHE INTERNAL "")
+  if(NOT ${VALA_VERSION} STREQUAL ${VALA_SHORTVER})
+	math(EXPR min_ver "${min_ver} + 2")
+	set(VALA_SHORTVER "${maj_ver}.${min_ver}" CACHE INTERNAL "")
+  endif()
   if(NOT "${maj_ver}" STREQUAL "" AND NOT "${min_ver}" STREQUAL "")
     set(VALA_LIBPKG "libvala-${VALA_SHORTVER}" CACHE INTERNAL "")
 

data/buildsystems/cmake/buildsystem/cmake/vala/FindVala.cmake
@@ -73,7 +73,7 @@
 ##
 
 # Search for the valac executable in the usual system paths.
-find_program(VALA_EXECUTABLE NAMES "valac" "valac-0.22" "valac-0.20" "valac-0.18" "valac-0.16" "valac-0.14" "valac-0.12")
+find_program(VALA_EXECUTABLE NAMES "valac" "valac-0.26" "valac-0.24" "valac-0.22" "valac-0.20" "valac-0.18")
 mark_as_advanced(VALA_EXECUTABLE)
 
 # Determine the valac version

CMakeLists.txt
@@ -47,7 +47,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/vala")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
 
 
-find_package(Vala "0.17" REQUIRED)
+find_package(Vala "0.20" REQUIRED)
 
 
 option(TRANSLATION "Build translation." ON)
@@ -95,29 +95,44 @@ else()
   list(APPEND definitions "NOT_WIN32")
 endif()
 # libvala-X.Y
-if(VALA_SHORTVER STREQUAL "0.18")
-  list(REMOVE_ITEM required_pkgs "libvala-0.20")
+if(VALA_SHORTVER STREQUAL "0.20")
   list(REMOVE_ITEM required_pkgs "libvala-0.22")
-elseif(VALA_SHORTVER STREQUAL "0.20")
-  list(REMOVE_ITEM required_pkgs "libvala-0.18")
-  list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
   list(APPEND definitions "VALAC_0_20")
 elseif(VALA_SHORTVER STREQUAL "0.22")
-  list(REMOVE_ITEM required_pkgs "libvala-0.18")
   list(REMOVE_ITEM required_pkgs "libvala-0.20")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
   list(APPEND definitions "VALAC_0_20")
-else()
-  list(REMOVE_ITEM required_pkgs "libvala-0.18")
+elseif(VALA_SHORTVER STREQUAL "0.24")
   list(REMOVE_ITEM required_pkgs "libvala-0.20")
   list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
+  list(APPEND definitions "VALAC_0_20")
+elseif(VALA_SHORTVER STREQUAL "0.26")
+  list(REMOVE_ITEM required_pkgs "libvala-0.20")
+  list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(APPEND definitions "VALAC_0_20")
+else()
+  list(REMOVE_ITEM required_pkgs "libvala-0.20")
+  list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
   list(APPEND required_pkgs "libvala-${VALA_SHORTVER}")
   list(APPEND definitions "VALAC_0_20")
 endif()
 # gtk+-3.0
-pkg_check_modules(GTK3.0 REQUIRED "gtk+-3.0 >= 3.4")
-compare_version_strings("3.6" "${GTK3.0_VERSION}" gtkres)
+pkg_check_modules(GTK3.0 REQUIRED "gtk+-3.0 >= 3.10")
+message(STATUS "gtk version : ${GTK3.0_VERSION}")
+if((${GTK3.0_VERSION} GREATER 3.12) OR (${GTK3.0_VERSION} EQUAL 3.12))
+	message(STATUS "SourceGutter is deprecated. Use get_window (WindowType type) on view directly.")
+	list(APPEND definitions "GTK_3_12")
+endif()
+compare_version_strings("3.10" "${GTK3.0_VERSION}" gtkres)
 if(gtkres LESS 1)
-  list(APPEND definitions "GTK_3_6")
+  list(APPEND definitions "GTK_3_10")
 endif()
 # gdl-3.0
 set(gdl_workarround OFF)

cmake/guanako.cmake
@@ -6,9 +6,10 @@ set(required_pkgs
   "glib-2.0"
   "gio-2.0"
   "gee-0.8 >= 0.10.5"
-  "libvala-0.18"
   "libvala-0.20"
   "libvala-0.22"
+  "libvala-0.24"
+  "libvala-0.26"
   "libxml-2.0"
   "posix {nocheck,nolink}"
 )

cmake/project.cmake
@@ -19,9 +19,10 @@ set(required_pkgs
   "gtksourceview-3.0"
   "guanako"
   "json-glib-1.0 {nolink}"
-  "libvala-0.18"
   "libvala-0.20"
   "libvala-0.22"
+  "libvala-0.24"
+  "libvala-0.26"
   "libxml-2.0"
   "pango {nolink}"
   "pangocairo {nolink}"

data/buildsystems/cmake/buildsystem/CMakeLists.txt
@@ -48,7 +48,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/vala")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
 
 
-find_package(Vala "0.17" REQUIRED)
+find_package(Vala "0.20" REQUIRED)
 
 # Custom library version checks.
 set(definitions)

guanako/CMakeLists.txt
@@ -56,7 +56,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/vala")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
 
 
-find_package(Vala "0.17" REQUIRED)
+find_package(Vala "0.20" REQUIRED)
 
 
 set(pc_prefix ${CMAKE_INSTALL_PREFIX})
@@ -72,21 +72,31 @@ set(pc_version "${${project_name}_VERSION}")
 set(definitions)
 find_package(PkgConfig)
 # libvala-X.Y
-if(VALA_SHORTVER STREQUAL "0.18")
-  list(REMOVE_ITEM required_pkgs "libvala-0.20")
+if(VALA_SHORTVER STREQUAL "0.20")
   list(REMOVE_ITEM required_pkgs "libvala-0.22")
-elseif(VALA_SHORTVER STREQUAL "0.20")
-  list(REMOVE_ITEM required_pkgs "libvala-0.18")
-  list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
   list(APPEND definitions "VALAC_0_20")
 elseif(VALA_SHORTVER STREQUAL "0.22")
-  list(REMOVE_ITEM required_pkgs "libvala-0.18")
   list(REMOVE_ITEM required_pkgs "libvala-0.20")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
   list(APPEND definitions "VALAC_0_20")
-else()
-  list(REMOVE_ITEM required_pkgs "libvala-0.18")
+elseif(VALA_SHORTVER STREQUAL "0.24")
   list(REMOVE_ITEM required_pkgs "libvala-0.20")
   list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
+  list(APPEND definitions "VALAC_0_20")
+elseif(VALA_SHORTVER STREQUAL "0.26")
+  list(REMOVE_ITEM required_pkgs "libvala-0.20")
+  list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(APPEND definitions "VALAC_0_20")
+else()
+  list(REMOVE_ITEM required_pkgs "libvala-0.20")
+  list(REMOVE_ITEM required_pkgs "libvala-0.22")
+  list(REMOVE_ITEM required_pkgs "libvala-0.24")
+  list(REMOVE_ITEM required_pkgs "libvala-0.26")
   list(APPEND required_pkgs "libvala-${VALA_SHORTVER}")
   list(APPEND definitions "VALAC_0_20")
 endif()

po/de.po
@@ -1836,6 +1836,10 @@ msgstr ""
 "Unbekannter Wert für Ersetzungseigenschaft '%s' in Zeile %hu (vermute '%s'): "
 "%s"
 
+#: ../src/ui/project_browser.vala:304
+msgid "User interface files"
+msgstr ""
+
 #: ../build/data/valama.desktop.in.h:4
 msgid "Vala;IDE;Development;"
 msgstr "Vala;IDE;Entwicklung;Entwicklungsumgebung;"

commit 379754420e9c3443a2753e6c761839bb08592edf

CMake: Search for global valac first.
 - Fix regression introduced by 19347ef. Don't try to find a smarter solution
   than the OS.

cmake/vala/FindVala.cmake
@@ -73,7 +73,7 @@
 ##
 
 # Search for the valac executable in the usual system paths.
-find_program(VALA_EXECUTABLE NAMES "valac-0.26" "valac-0.24" "valac-0.22" "valac-0.20" "valac")
+find_program(VALA_EXECUTABLE NAMES "valac" "valac-0.26" "valac-0.24" "valac-0.22" "valac-0.20")
 mark_as_advanced(VALA_EXECUTABLE)
 
 # Determine the valac version