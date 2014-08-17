Valama/valama

commit f3fcc1e202d40e76e986a96f11d29e54ac0a55a2

Valama: Fix plain build system.
 * Set buildpath in preparation method.
 * Use base method in BuilderCMake and BuilderAutotools as well.

src/buildsystem/autotools.vala
@@ -27,8 +27,8 @@ public class BuilderAutotools : BuildSystem
 	}
 	
 	public override bool preparate() throws BuildError.INITIALIZATION_FAILED {
-        if (project == null)
-            throw new BuildError.INITIALIZATION_FAILED (_("Valama project not initialized."));
+        if (!base.preparate())
+            return false;
         configure_ac = Path.build_path (Path.DIR_SEPARATOR_S,
                                        project.project_path,
                                        "configure.ac");
@@ -37,7 +37,6 @@ public class BuilderAutotools : BuildSystem
                                        "Makefile.am");
         if (prepared)
 			return true;
-        buildpath = Path.get_dirname (configure_ac);  
         init_dir (Path.build_path (Path.DIR_SEPARATOR_S, buildpath, "m4"));
         prepared = true;
         return true;

src/buildsystem/base.vala
@@ -424,12 +424,14 @@ public abstract class BuildSystem : Object {
     }
 
     public virtual bool preparate() throws BuildError.INITIALIZATION_FAILED {
-        if (buildpath == null)
-            // TRANSLATORS:
-            // The user has to choose a directory where to build the application
-            // so it might be undefined.
-            throw new BuildError.INITIALIZATION_FAILED (_("Build directory not set."));
-        init_dir (buildpath);
+        if (buildpath == null) {
+            if (project == null)
+                throw new BuildError.INITIALIZATION_FAILED (_("Valama project not initialized."));
+            buildpath = Path.build_path (Path.DIR_SEPARATOR_S,
+                                         project.project_path,
+                                         "build");
+            init_dir (buildpath);
+        }
         return true;
     }
 

src/buildsystem/cmake.vala
@@ -45,16 +45,12 @@ public class BuilderCMake : BuildSystem {
     }
 
     public override bool preparate() throws BuildError.INITIALIZATION_FAILED {
-        if (project == null)
-            throw new BuildError.INITIALIZATION_FAILED (_("Valama project not initialized."));
-        buildpath = Path.build_path (Path.DIR_SEPARATOR_S,
-                                       project.project_path,
-                                       "build");
+        if (!base.preparate())
+            return false;
         projectinfo = Path.build_path (Path.DIR_SEPARATOR_S,
                                        project.project_path,
                                        "cmake",
                                        "project.cmake");
-        init_dir (buildpath);
         init_dir (Path.get_dirname (projectinfo));
         return true;
     }

commit a3d3545e77b2338810d8938f240e277456d4d1c3

Valama Autotools: Fix build path.
 * Bug introduced by f3fcc1e.

src/buildsystem/autotools.vala
@@ -26,14 +26,16 @@ public class BuilderAutotools : BuildSystem
 	}
 	
 	public override bool preparate() throws BuildError.INITIALIZATION_FAILED {
-        if (!base.preparate())
-            return false;
+        if (project == null)
+            throw new BuildError.INITIALIZATION_FAILED (_("Valama project not initialized."));
+        buildpath = project.project_path;
         configure_ac = Path.build_path (Path.DIR_SEPARATOR_S,
                                        project.project_path,
                                        "configure.ac");
         makefile_am = Path.build_path (Path.DIR_SEPARATOR_S,
                                        project.project_path,
                                        "Makefile.am");
+        init_dir (buildpath);
         init_dir (Path.build_path (Path.DIR_SEPARATOR_S, buildpath, "m4"));
         return true;
 	}